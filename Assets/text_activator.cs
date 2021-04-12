b12f1ff7a38b7a666f8e3b2b746dddd73507d0e3
bubby1914 <54813316+bubby1914@users.noreply.github.com>
2021-04-12 12:28:46 -0400

    adding dialouge system

---
+++ Assets/text_activator.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class text_activator : MonoBehaviour
+{
+    public GameObject text;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            text.SetActive(true);
+        }
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            text.SetActive(false);
+        }
+    }
+}
