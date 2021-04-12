4fc71ef5ee57d70eee67b0b1286e75efd488187b
Leo Current <78365840+milkmoth@users.noreply.github.com>
2021-04-12 02:46:14 -0400

    meow

---
+++ Assets/movement assets/player.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player : MonoBehaviour
+{
+    public float movementSpeed = 8f;
+    public float jumpSpeed = 12f;
+    private Rigidbody2D rigidBody;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.W)) //help from codemahal
+        {
+            rigidBody.velocity = new Vector2(rigidBody.velocity.x, jumpSpeed);
+        }
+    }
+    // Update is called once per frame
+    void FixedUpdate() //updates w the jump mechanics. because fixed update is with physics, i do it here rather than in an update
+    {
+        Vector3 walkingMovement = new Vector3(Input.GetAxis("Horizontal"), 0f, 0f); //horizontal movement vector
+        transform.position += walkingMovement * Time.deltaTime * movementSpeed; //estabilishes the movement
+    }
+}
+
